a957dae51f31d61d0791e13181ba9f77399516aa
ilyamoiseenko <ilya.moiseenko.00@yandex.by>
2024-09-18 21:28:57 +0300

    Implement CountryService

---
+++ src/main/java/by/moiseenko/ecommerce/domain/dto/request/CountryRequest.java
@@ -0,0 +1,13 @@
+package by.moiseenko.ecommerce.domain.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CountryRequest {
+
+    private String name;
+}
---
+++ src/main/java/by/moiseenko/ecommerce/domain/dto/response/CountryResponse.java
@@ -0,0 +1,13 @@
+package by.moiseenko.ecommerce.domain.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CountryResponse {
+
+    private String name;
+}
